850f3e29712ca80d947fd3319bc3c16521ee81a1
Catalina Valdivia <catalina.valdivia@student.ecp.fr>
2020-11-10 16:39:14 +0100

    Optimistic fn for Anant's and my fn

--- 3.1.py
+++ 3.1.py
@@ -208,12 +208,108 @@ def Pess_1(subset, pi, w, threshold):
     New_Subset.to_csv("New_Subseeet.csv", header=True)
 
 
-def OptimisticmajoritySorting():
-    pass
+def Optimistic_Anant(subset, pi, w, threshold):
+    w_sum = sum(w.values())
+    maximize = ['fiber100g','proteins100g']
+    New_Op_Subset = subset.copy()
+    
+    pi = pi[::-1]  #reverse the dataframe
+    pi.reset_index(inplace=True)
+    print(pi)
+    
+    for threshold in [0.5, 0.6, 0.7]:
+        New_Op_Subset['pessimistic_grade_'+str(threshold)] = ""
+    for index, tup in subset.iterrows():
+        print('\n\nindex= ',index)
+        for p_index, value in (pi.iterrows()):
+            print('\npi =', value.pi)
+            s = 0.
+            for crit in subset.columns[-6:]: # the <= => change
+                if crit in maximize:
+                    if tup[crit] <= value[crit]:
+                        s += w[crit]
+                        print('min = ',crit)
+                        print('s =',s)
+                else:
+                    if tup[crit] >= value[crit]:
+                        s += w[crit]
+                        print('max = ',crit)
+                        print('s =',s)
+            diff = s / w_sum
+            print('diff =', diff)
+            for threshold in [0.5, 0.6, 0.7]:
+                if not New_Op_Subset.loc[index,'pessimistic_grade_'+str(threshold)]\
+                    and diff > threshold: # Had to change
+                    if p_index in [4,5]:
+                        grade = 'a'
+                    elif p_index == 3:
+                        grade ='b'
+                    elif p_index == 2:
+                        grade = 'c'
+                    elif p_index == 1:
+                        grade ='d'
+                    elif p_index == 0:
+                        grade = 'e'
+                    New_Op_Subset.loc[index,'pessimistic_grade_'+str(threshold)] = grade
+                    continue
+
+    New_Op_Subset.to_csv("New_Op_Anant.csv", header=True)
 
 
 
-
+def Optimistic_Cata(subset, pi, w, threshold):
+    w_sum = sum(w.values())
+    passpi = pd.DataFrame(index=subset.index, columns=pi.pi)
+    maximize = ['fiber100g','proteins100g']
+    New_Op_Subset = subset.copy()
+    
+    pi = pi[::-1]  #reverse the dataframe
+    pi.reset_index(inplace=True)
+    print(pi)
+#    for threshold in [0.5, 0.6, 0.7]:
+#        New_Subset['pessimistic_grade_'+str(threshold)] = ""
+    for ix, name in enumerate(subset.productname):
+        print('\nprod =', name)
+        for p in pi.pi:
+            s = 0
+            print('\npi =',p)
+            for crit in subset.columns[-6:]:
+                if crit not in maximize:
+                    if subset[crit][ix] >= pi[pi.pi==p][crit].values:
+                        s += w[crit]
+                        print('min =', crit)
+                        print('s =', s)
+                else:
+                    if subset[crit][ix] <= pi[pi.pi==p][crit].values:
+                        s += w[crit]
+                        print('max =', crit)
+                        print('s =', s)
+                
+            if s/w_sum > threshold: # CHANGES FROM >= to >
+                passpi.loc[ix,p] = True
+                break
+            else:
+                passpi.loc[ix,p] = False
+            
+    for ix, line in passpi.iterrows():
+        for pi in line.index:
+            if line[pi] == True: 
+                if pi == 'pi6' or pi == 'pi5':
+                    grade = 'a'
+                elif pi == 'pi4':
+                    grade = 'b'
+                elif pi == 'pi3':
+                    grade = 'c'
+                elif pi == 'pi2':
+                    grade = 'd'
+                else:
+                    grade = 'e'
+                print('grade =', grade)
+                New_Op_Subset.loc[ix,'optimistic_grade_'+str(threshold)] = grade
+            elif line[pi] == None:
+                break
+#        assert False
+    return passpi
 
 
 
@@ -234,5 +330,7 @@ if __name__ == '__main__':
 
 
     # function calls
-    # Pess_1(df, pi, w, threshold)
-    additive()
+#    Pess_1(df, pi, w, [0.5,0.6,0.7])
+#    additive()
+    Optimistic_Anant(subset, pi, w, [0.5,0.6,0.7])
+#    Optimistic_Cata(subset, pi, w, 0.5)
